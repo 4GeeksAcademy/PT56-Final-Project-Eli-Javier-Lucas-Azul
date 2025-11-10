bde568fb095819fdb4eac12ca9b5915c9d560c2d
Lucas Urquiza <105996657+LucasUr11@users.noreply.github.com>
2025-11-10 18:13:09 +0000

    Cambios de estilos en el 'Navbar'

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
 
 	return (
-		<nav className="navbar navbar-light bg-light">
+		<nav className="navbar">
 			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+				<div className="">Icono de la app</div>
+				<div className="">
+					<h4>Billetera Familiar</h4>
+				</div>
+				<div className="">
+					<button className="btn btn-navbar">Cerrar Sesión</button>
+					{/* Agregar un icono al boton */}
 				</div>
 			</div>
 		</nav>
