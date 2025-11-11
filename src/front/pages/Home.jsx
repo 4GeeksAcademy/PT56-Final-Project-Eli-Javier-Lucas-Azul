61d22dd67858ccbf3df18ea30f6320382d5b5687
Lucas Urquiza <105996657+LucasUr11@users.noreply.github.com>
2025-11-11 01:06:45 +0000

    Agregue un formulario en el cuerpo de 'Home.jsx'

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -4,48 +4,30 @@ import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
 export const Home = () => {
 
-	const { store, dispatch } = useGlobalReducer()
-
-	const loadMessage = async () => {
-		try {
-			const backendUrl = import.meta.env.VITE_BACKEND_URL
-
-			if (!backendUrl) throw new Error("VITE_BACKEND_URL is not defined in .env file")
-
-			const response = await fetch(backendUrl + "/api/hello")
-			const data = await response.json()
-
-			if (response.ok) dispatch({ type: "set_hello", payload: data.message })
-
-			return data
-
-		} catch (error) {
-			if (error.message) throw new Error(
-				`Could not fetch the message from the backend.
-				Please check if the backend is running and the backend port is public.`
-			);
-		}
-
-	}
-
-	useEffect(() => {
-		loadMessage()
-	}, [])
-
 	return (
-		<div className="text-center mt-5">
-			<h1 className="display-4">Hello Rigo!!</h1>
-			<p className="lead">
-				<img src={rigoImageUrl} className="img-fluid rounded-circle mb-3" alt="Rigo Baby" />
-			</p>
-			<div className="alert alert-info">
-				{store.message ? (
-					<span>{store.message}</span>
-				) : (
-					<span className="text-danger">
-						Loading message from the backend (make sure your python 🐍 backend is running)...
-					</span>
-				)}
+		<div className="container mt-5 d-flex flex-column">
+			<div className="text-center">
+				<h1>Registrate</h1>
+			</div>
+			<div>
+				<form className="card p-3">
+					<div className="mb-3">
+						<label for="name" className="form-label">Nombre</label>
+						<input type="text" className="form-control" id="name" aria-describedby="name" />
+					</div>
+					<div className="mb-3">
+						<label for="email" className="form-label">Email</label>
+						<input type="email" className="form-control" id="email" />
+					</div>
+					<div className="mb-3">
+						<label for="password" className="form-label">Contraseña</label>
+						<input type="password" className="form-control" id="password" />
+					</div>
+					<button type="submit" className="btn btn-primary">Submit</button>
+				</form>
+			</div>
+			<div>
+				<p>¿Ya tienes una cuenta? Ingresa aquí</p>
 			</div>
 		</div>
 	);
